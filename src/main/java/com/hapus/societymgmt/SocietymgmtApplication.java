c2e99a17da680c7d36229cbdcf1d70521c45f13c
Sudeep Ambekar <sudeep@Sudeeps-MacBook-Pro.local>
2018-08-24 22:15:07 +0200

    Initial commit

---
+++ src/main/java/com/hapus/societymgmt/SocietymgmtApplication.java
@@ -0,0 +1,12 @@
+package com.hapus.societymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietymgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietymgmtApplication.class, args);
+	}
+}
